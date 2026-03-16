54bcca91135b78a39a5725897f90a4043a432edc
vdrews <vanessa.andrews876@gmail.com>
2026-03-16 14:05:41 -0500

    updated log

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 from flask import Flask, request, jsonify
 import mysql.connector
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
-
-import os
-
 
 db = mysql.connector.connect(
     host=os.environ.get("mysql.railway.internal"),
     port=int(os.environ.get("3306", 3306)),
     user=os.environ.get("root"),
-    password=os.environ.get("axqVSWVLvSjIejhlSuIgIRYsGDCZgNGZ"),
+    password=os.environ.get("DaxqVSWVLvSjIejhlSuIgIRYsGDCZgNGZ"),
     database=os.environ.get("demo_app")
 )
 cursor = db.cursor()
@@ -21,30 +19,24 @@ cursor = db.cursor()
 def home():
     return "welcome home"
 
-
-# Add student
 @app.route('/add-student', methods=['POST'])
 def add_student():
-
     data = request.json
     name = data['name']
     email = data['email']
 
     sql = "INSERT INTO students (name, email) VALUES (%s,%s)"
-    cursor.execute(sql,(name,email))
+    cursor.execute(sql, (name, email))
     db.commit()
 
-    return jsonify({"message":"Student added"})
+    return jsonify({"message": "Student added"})
 
-# Get all students
 @app.route('/students', methods=['GET'])
 def get_students():
-
     cursor.execute("SELECT * FROM students")
     result = cursor.fetchall()
 
     students = []
-
     for row in result:
         students.append({
             "id": row[0],
@@ -53,5 +45,3 @@ def get_students():
         })
 
     return jsonify(students)
-
-app.run(debug=True)
